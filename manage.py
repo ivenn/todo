58b7e7d9437b0ad6a225c94b30d64f93971a26a3
Va11ter <va77ter@gmail.com>
2015-09-20 22:59:31 +0300

    Somebody submit tabs instead spaces to manage.py. Fixed

--- manage.py
+++ manage.py
@@ -5,20 +5,20 @@ manager = Manager(app)
 
 @manager.command
 def create_db():
-	db.create_all()
+    db.create_all()
 
 @manager.command
 def drop_db():
-	db.drop_all()
+    db.drop_all()
 
 @manager.command
 def create_admin():
-	from app.models import User
-	User.add_user(User(username='admin',
-		password='admin',
-		is_admin=True,
-		email='admin@toDo.com',
-		confirmed=True))
+    from app.models import User
+    User.add_user(User(username='admin',
+        password='admin',
+        is_admin=True,
+        email='admin@toDo.com',
+        confirmed=True))
 
 
 if __name__ == "__main__":
